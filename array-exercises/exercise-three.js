ef9a13f6daf905c8332a93fe8eb0e3e6d02d101a
tomburger64 <tomlazeryop.com@gmail.com>
2024-09-09 12:43:37 +0200

    exercise 3

--- array-exercises/exercise-three.js
+++ array-exercises/exercise-three.js
@@ -1,21 +1,47 @@
+// https://www.youtube.com/watch?v=tFz502-LXU8&list=PLpc_YvcwbxaSX4QlwXZ_vjBAZA4cNgIxN&index=7
 // 3) Get a list of the people's full name (firstName and lastName).
 
 const people = [
-    { firstName: 'Sam', lastName: 'Hughes', DOB: '07/07/1978', department: 'Development', salary: '45000' },
-    { firstName: 'Terri', lastName: 'Bishop', DOB: '02/04/1989', department: 'Development', salary: '35000' },
-    { firstName: 'Jar', lastName: 'Burke', DOB: '11/01/1985', department: 'Marketing', salary: '38000' },
-    { firstName: 'Julio', lastName: 'Miller', DOB: '12/07/1991', department: 'Sales', salary: '40000' },
-    { firstName: 'Chester', lastName: 'Flores', DOB: '03/15/1988', department: 'Development', salary: '41000' },
-    { firstName: 'Madison', lastName: 'Marshall', DOB: '09/22/1980', department: 'Sales', salary: '32000' },
-    { firstName: 'Ava', lastName: 'Pena', DOB: '11/02/1986', department: 'Development', salary: '38000' },
-    { firstName: 'Gabriella', lastName: 'Steward', DOB: '08/26/1994', department: 'Marketing', salary: '46000' },
-    { firstName: 'Charles', lastName: 'Campbell', DOB: '09/04/1977', department: 'Sales', salary: '42000' },
-    { firstName: 'Tiffany', lastName: 'Lambert', DOB: '05/11/1990', department: 'Development', salary: '34000' },
-    { firstName: 'Antonio', lastName: 'Gonzalez', DOB: '03/24/1985', department: 'Office Management', salary: '49000' },
-    { firstName: 'Aaron', lastName: 'Garrett', DOB: '09/04/1985', department: 'Development', salary: '39000' },
+    { firstName: 'Sam', lastName: 'Hughes'},
+    { firstName: 'Terri', lastName: 'Bishop'},
+    { firstName: 'Jar', lastName: 'Burke'},
+    { firstName: 'Julio', lastName: 'Miller'},
+    { firstName: 'Chester', lastName: 'Flores'},
+    { firstName: 'Madison', lastName: 'Marshall'},
+    { firstName: 'Ava', lastName: 'Pena'},
+    { firstName: 'Gabriella', lastName: 'Steward'},
+    { firstName: 'Charles', lastName: 'Campbell'},
+    { firstName: 'Tiffany', lastName: 'Lambert'},
+    { firstName: 'Antonio', lastName: 'Gonzalez'},
+    { firstName: '', lastName: 'Gonzalez'},
+    { firstName: 'Aaron', lastName: 'Garrett'},
+    { firstName: 'Aaron', lastName: ''},
 ];
 
+let fullName = people.map((names) => names.firstName + " " + names.lastName)
+// console.log(fullName)
 
+//since it's really simple I'll try to do it with filter
+// first time using filter, had to use chatgpt to explain me
+let fullName2 = people.filter((names) => {
+    if (names.firstName && names.lastName){
+        console.log(names.firstName + " " + names.lastName)
+    }
+})
+// no need to specify anything to return true or false, just mention vars / functions
+
+
+console.log("-----")
+//correction
+console.log(people.map(person => `${person.firstName} ${person.lastName}`))
+
+//destrucuring
+console.log(people.map(({firstName, lastName}) => `${firstName} ${lastName}`))
+//I get it but I've barely used destructuring so far, I guess you can use it as an arrow function
+
+// using spread opperator, making a new object and fullName propriety
+console.log(people.map(person => ({...person, fullName: `${person.firstName} ${person.lastName}`})))
+//↑ this one doesn't check if the person's got a first name and last name though
 
 // 4) Get a list of people in the array ordered from youngest to oldest.
 // 5) How many people are there in each department?
