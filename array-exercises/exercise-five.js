7e504a3ea8764e4568a9851cce7439cf5232c742
tomburger64 <tom64bonde@gmail.com>
2024-09-13 15:06:55 +0200

    exercise 5 done

--- array-exercises/exercise-five.js
+++ array-exercises/exercise-five.js
@@ -21,28 +21,35 @@ const people = [
 // 1 make an array of deps
 
 // 2 make a function
-// 3 make a var that filters each value of the array
-// 4 look for that "each value" to see how many times it's in the array
+// 3 filter each value of the array
+// 4 look for that "each value" to count how many times it's in the array
 
 const deps = people.map((x) => x.department)
 
-function howManyDeps(){
+// had to cheat to figure out how to count the deps without having them pre-registered but rather detected from the deps array directly (see fails at the bottom of this code)
 
-    //for each array element, returns that element
-    let department = deps.map((department) => department)
+//didn't think abt using an object to store data, thought about getting the dep name as a variable and adding +1 everytime it's detected or something like that
 
-    //looks for filterElement in the array, return filterElement and the amount of times it's filtered in the array
+//I don't have the right logic yet
 
-    deps.filter(department => {
-        //if department is there more than once in the array
-        //had to cheat to know you can use .length to know how many times department is in the array
-        if (){
-            
-        }
+function depCount(){
+    let depCount = {}
+    //object to store the dep name AND its count
+
+    deps.forEach(department => {
+    //for each value in the deps array
+        const count = deps.filter(dep => dep === department).length
+        //filter creates a new array, adding the department (dep) everytime it's equal to itself (department, same thing but through the forEach), and counts itself everytime it's detected in the array
+        //comparing department to itself indirectly is necessary so that we can keep the count of every instance of each department
+
+        depCount[department] = count
+        //stores the count of the dep in the depcount object
     })
-
+    console.log(depCount)
 }
-howManyDeps()
+depCount()
+
+// took too much time doing this to end up doing the braining part with AI so I'll just do more exercises to train later
 
 
 
@@ -74,3 +81,26 @@ howManyDeps()
 // console.log(whatDep())
 
 //this  probably could've worked if I fixed it but I'd rather "detect" what department is there istead of recognizing it
+
+
+
+// 1 make an array of deps
+// 2 make a function
+// 3 make a var that filters each value of the array
+// 4 look for that "each value" to see how many times it's in the array
+// function howManyDeps(){
+
+//     //for each array element, returns that element
+//     let department = deps.map((department) => department)
+
+//     //looks for filterElement in the array, return filterElement and the amount of times it's filtered in the array
+
+//     deps.filter(department => {
+//         //if department is there more than once in the array
+//         if (){
+            
+//         }
+//     })
+
+// }
+// howManyDeps()
