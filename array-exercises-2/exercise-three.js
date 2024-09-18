3315aa0d8b582750f29fefec5dbcf21e00bde29d
Tom <tom64bonde@gmail.com>
2024-09-18 15:34:17 +0200

    struggling with the approach but on the right path

--- array-exercises-2/exercise-three.js
+++ array-exercises-2/exercise-three.js
@@ -23,3 +23,53 @@ const orders = [
 ];
 
 // 3) Have all the orders been delivered?
+console.log("should be 'true false true true true'")
+
+
+
+const deliveriesData = orders.map((x) => x.delivered === true) //returns true for every instance of delivered = true
+console.log(deliveriesData)
+
+
+//same thing but reverse, returns true when has not been delivered
+const hasDelivered = deliveriesData.map((x) => x === false)
+console.log(hasDelivered)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fails
+
+
+
+//look through every instance of this array
+// const hasDelivered = orders.map((x) => {
+
+    //use reduce to compile every state for delivered and check if it's all true
+    // let states = x.reduce((acc, state) => acc + state.delivered)
+    // if (states.map((y) === true)){
+        // console.log("all true")
+    // }
+    // else{
+        // console.log("not all true")
+    // }
+// })
+
+
+
+// ---
+
+// const deliverStates = orders.reduce((acc, state) => [acc + "," + state.delivered])
+
+// const hasDelivered = deliverStates.map((x) => x === true ? true : false )
+// console.log(hasDelivered)
