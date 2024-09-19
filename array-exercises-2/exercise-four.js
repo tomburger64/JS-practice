a0b0ec0033de6c424ec7bb30500c8d38c4b5f248
Tom <tom64bonde@gmail.com>
2024-09-19 15:31:19 +0200

    exercise four done

--- array-exercises-2/exercise-four.js
+++ array-exercises-2/exercise-four.js
@@ -7,21 +7,29 @@ const orders = [
     ]},
     { orderId: '234', customerId: '234', deliveryDate: '01-02-2020', delivered: false, items: [
         { productId: '234', price: 30 },
-    ]},
-    { orderId: '345', customerId: '234', deliveryDate: '05-01-2020', delivered: true, items: [
-        { productId: '567', price: 30 },
-        { productId: '678', price: 80 },
-    ]},
-    { orderId: '456', customerId: '345', deliveryDate: '12-01-2020', delivered: true, items: [
-        { productId: '789', price: 12 },
-        { productId: '890', price: 90 },
-    ]},
-        { orderId: '578', customerId: '456', deliveryDate: '12-01-2020', delivered: true, items: [
-        { productId: '901', price: 43 },
-        { productId: '123', price: 55 },
-    ]},
+    ]}
 ];
 
 // 4) Has the customer with ID '123' made any orders?
 
-//test
+//what shows if there's an order has been made precisely
+// > if there's 1 >= objects in the items array
+
+console.log(orders.map((x) => x.items.length >= 1))
+//works for every order, returns true or false in order if one order doesn't have any object in items
+
+console.log(orders.map((x) => x.customerId === '123' && x.items.length >= 1))
+//works but still returns a value for the others
+
+const orderId = '123'
+const order123 = orders.filter((x) => x.customerId === orderId)
+console.log(order123.map((hasOrder) => hasOrder.items.length >= 1 ? `order ${orderId} has made orders` : `order ${orderId} has NOT made any order`))
+//please comment the customer 123's items' objects if you want to see the if statement work
+
+
+
+
+//fails ↓
+
+// const hasOrder = orders.items.map((x) => x.orderId === true )
+// console.log(hasOrder)
