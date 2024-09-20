ec734d88d1316a29e6d0724c6f73422476770219
Tom <tom64bonde@gmail.com>
2024-09-20 14:48:00 +0200

    ex 5 setup

--- array-exercises-2/exercise-five.js
+++ array-exercises-2/exercise-five.js
@@ -1,1 +1,25 @@
+// https://youtu.be/QsUSmmEf1JE?si=WiUDuvbyULQDMOCJ&t=354
+
+const orders = [
+    { orderId: '123', customerId: '123', deliveryDate: '01-01-2020', delivered: true, items: [
+        { productId: '123', price: 55 },
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '234', customerId: '234', deliveryDate: '01-02-2020', delivered: false, items: [
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '345', customerId: '234', deliveryDate: '05-01-2020', delivered: true, items: [
+        { productId: '567', price: 30 },
+        { productId: '678', price: 80 },
+    ]},
+    { orderId: '456', customerId: '345', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '789', price: 12 },
+        { productId: '890', price: 90 },
+    ]},
+        { orderId: '578', customerId: '456', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '901', price: 43 },
+        { productId: '123', price: 55 },
+    ]},
+];
+
 // 5) Have any products with an id of 123 been sold?
