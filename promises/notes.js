ae86bfc0b3f67492f5c95ec4ef9b6585b5ac7e8d
tomburger64 <tom64bonde@gmail.com>
2024-09-30 18:25:31 +0200

    more notes, need to finish commenting this

--- promises/notes.js
+++ promises/notes.js
@@ -45,3 +45,49 @@ function gotErr(err){
 // catch returns its callback when a promise is false
 
 // WHY USE CATCH => catch works and will callback even if then() itself doesn't work
+
+
+
+
+// compact version of the example
+// in a single line :
+fetch(API)
+.then(data => console.log(data))
+.catch(err => console.log(err));
+// fetch(API).then((data) => console.log(data)).catch((err) => console.log(err))
+
+// fetch makes a promise of API, and as it returns as true logs the data, or err if it's false
+
+
+
+// using .json() also makes promises
+// https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+
+fetch(API)
+.then(response => {
+    return response.json();
+})
+.then(json => {
+    createP(json.word);
+    return fetch(giphyAPI + json.word);
+})
+.then(response => {
+    return response.json();
+})
+.then(json => {
+    createImageBitmap(json.data[0].images['fixed_height_small'].url);
+})
+.catch(err => console.log(err));
+
+// WHY USE JSON() ?
+// json() returns the data as a JS object
+// simply returning it using then returns "raw" data, which can be binary or text
+
+// fetch the api
+// convert it into json
+// create a paragraph (createP) by specifying wwhat to put in (json.word)
+// (createP is probably deprecated)
+// fetch the api again
+// FINISH COMMENTING THIS ↑↑↑↑↑↑↑↑
+
+// not taking notes on what he does from this time stamp 18:40 because it apparently got errors and is mostly specific to the api showed as an example, when the goal is to understand (according to the guy from the video)
