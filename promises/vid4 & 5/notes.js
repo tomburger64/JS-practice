aa816b979c69d2460892ea553684b375d98b53ce
tomburger64 <tom64bonde@gmail.com>
2024-10-04 12:57:48 +0200

    no more chaining, parallel calling

--- promises/vid4 & 5/notes.js
+++ promises/vid4 & 5/notes.js
@@ -1,36 +1,57 @@
 // https://youtu.be/chavThlNz3s?si=2iDenitxB0B7N6u9 v4
 // https://youtu.be/01RTj1MWec0?si=EnpFYCYISxFU_Yxr v5
 
-const API = "APIlink"
+const wordnikAPI = "APIlink"
+const giphyAPI = "APIlink"
 
 function setup(){
 
     noCanvas(); //p5 func
 
-    wordGIF().then(results => {
+    // added in vid5, specify amount of chars called in wordnik api
+    wordGIF(3).then(results => {
         // if the wordGIF func returns as true, then create <p></p> with the word obj prop and <img></img> with the img object prop
         createP(results.word); // p5 function
         createImg(results.img); // p5 function
-        return wordGIF();
-        // return wordGIF func to use it twice ↓ (done in vid5)
-    })
-    .then(results => {
-        // again, if wordGIF's promise is successful ↓
-        // results contains the last wordGIF return right above ↑
-        createP(results.word); // p5 function
-        createImg(results.img); // p5 function)
-        // creates another p and img tag with a new word and image
     })
     .catch(err => console.error(err));
+    
+    // no more chaining, just doing them in parallel by calling them seperatly
+    wordGIF(4).then(results => {
+        createP(results.word); // p5 function
+        createImg(results.img); // p5 function
+    })
+    .catch(err => console.error(err));
+
+
+
+    // // chained version ↓
+    // // added in vid5, specify amount of chars called in wordnik api
+    // wordGIF(4).then(results => {
+    //     // if the wordGIF func returns as true, then create <p></p> with the word obj prop and <img></img> with the img object prop
+    //     createP(results.word); // p5 function
+    //     createImg(results.img); // p5 function
+    //     return wordGIF(5);
+    //     // return wordGIF func to use it twice ↓ (done in vid5)
+    // })
+    // .then(results => {
+    //     // again, if wordGIF's promise is successful in the new promise right above
+    //     // results contains wordGIF returned right above ↑
+    //     createP(results.word); // p5 function
+    //     createImg(results.img); // p5 function)
+    //     // creates another p and img tag with a new word and image
+    // })
+    // .catch(err => console.error(err));
 }
 
 // new async function (variant of oldWordGIF() below ↓ )
-async function wordGIF(){
+async function wordGIF(num){
 
-    let response1 = await fetch(API); // first fetch the api
+    // done in vid5, specify the amount of characters to the called word ↓
+    let response1 = await fetch(wordnikAPI + '&minLength='+num+'&maxLength='+num); // first fetch the api and specify the char limit
     let json = await response.json(); // convert the fetch req into json
     let word = json1.word; // define word var to be = to the word object prop (from the json1 object)
-    let response2 = await fetch(giphyAPI + json.word); // bad naming, but makes another fetch req to the api adding the word object prop to the req
+    let response2 = await fetch(giphyAPI + json1.word); // bad naming, but makes another fetch req to the api adding the word object prop to the req
     let json2 = await response2.json(); // convert that new req into json
     let img = json2.data[0].images['fixed_height_small'].url // also get the img from the json2 object
 
@@ -48,6 +69,7 @@ async function wordGIF(){
 
 
 // reminder: changing this ↓ into an async func (done above ↑)
+// const API = "api link"
 function oldWordGIF(){
     fetch(API)
     .then(response => response.json())
